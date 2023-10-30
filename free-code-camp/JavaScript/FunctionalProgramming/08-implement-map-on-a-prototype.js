c1a6019f4ef7088698e22716a65f04e67d2500b0
jasminehilton <jasminehilton1@live.com>
2023-10-30 16:32:39 -0700

    question 8 done

--- free-code-camp/JavaScript/FunctionalProgramming/08-implement-map-on-a-prototype.js
+++ free-code-camp/JavaScript/FunctionalProgramming/08-implement-map-on-a-prototype.js
@@ -7,3 +7,12 @@
 
 // Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). You should not use the built-in map method. The Array instance can be accessed in the myMap method using this.
 
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+  this.forEach((element, index, originalArr) =>
+    newArray.push(callback(element, index, originalArr))
+  )
+  // Only change code above this line
+  return newArray;
+};
