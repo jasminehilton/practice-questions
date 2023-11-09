d738d5914e33e02af94d873d32636fe06bafc317
jasminehilton <jasminehilton1@live.com>
2023-11-09 15:27:17 -0800

    added kata 1 to the new folder

---
+++ lighthouse/katas/01-sum-the-largest-numbers.js
@@ -0,0 +1,18 @@
+// In this exercise, you will be given an array of two or more numbers. You will then have to find the two largest numbers in that array, and add (sum) them together.
+
+// Input
+// const sumLargestNumbers = function(data) {
+//   // Put your solution here
+// };
+
+// console.log(sumLargestNumbers([1, 10]));
+// console.log(sumLargestNumbers([1, 2, 3]));
+// console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
+
+// Expected Output
+// 11
+// 5
+// 126
+
+// Create a function named sumLargestNumbers that will receive an array of numbers and return the sum of the two largest numbers in that array.
+
